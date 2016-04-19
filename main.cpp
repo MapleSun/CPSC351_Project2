997bcb3fd9bdbafc75a35bf6c6dd204f93e205a9
Joshua Womack <joshwomack@hotmail.com>
2016-04-18 18:15:31 -0700

    Setting up my own version

--- main.cpp
+++ main.cpp
@@ -5,52 +5,58 @@ using namespace std;
 
 int NUM_PROCESS;
 
-struct Process{
+struct Process
+{
     int processId;
     int arrivalTime;
     int lifeTime;
     int numOfPiece;
-    int* sizeOfMemChunk;
+    int *sizeOfMemChunk;
     
-    Process(int Id, int arrivalTime, int lifeTime, int numOfPiece, int* sizeOfMemChunk):processId(Id),arrivalTime(lifeTime),lifeTime(lifeTime),numOfPiece(numOfPiece), sizeOfMemChunk(sizeOfMemChunk){}
+    Process(int Id, int arrivalTime, int lifeTime, int numOfPiece, int* sizeOfMemChunk):processId(Id),arrivalTime(arrivalTime),lifeTime(lifeTime),numOfPiece(numOfPiece), sizeOfMemChunk(sizeOfMemChunk){}
     
-    void print(){
+    void print()
+    {
         cout << "ID: " << processId << endl
             << arrivalTime << " " << lifeTime << endl
         << numOfPiece;
-        for(int i=0; i<numOfPiece; i++){
+        for(int i=0; i<numOfPiece; i++)
+        {
             cout << " " << sizeOfMemChunk[i];
         }
         cout << endl;
     }
 };
 
-Process* fileReader(){
-    fstream fin;
-    fin.open("in1.txt", fstream::in);
-    fin >> NUM_PROCESS;
-    cout << NUM_PROCESS << endl;
-    Process* Processes[NUM_PROCESS];
+void fileReader(fstream& fin, Process *Processes[])
+{
     int processId;
     int arrivalTime;
     int lifeTime;
     int numOfPiece;
-    for(int i=0; i<NUM_PROCESS; i++){
+    for(int i=0; i<NUM_PROCESS; i++)
+    {
         fin >> processId >> arrivalTime >> lifeTime >> numOfPiece;
         int* sizeOfMemChunk = new int [numOfPiece];
-        for(int j = 0; j < numOfPiece; j++){
+        for(int j = 0; j < numOfPiece; j++)
+        {
             fin >> sizeOfMemChunk[j];
         }
         Processes[i] = new Process(processId, arrivalTime, lifeTime, numOfPiece, sizeOfMemChunk);
         Processes[i]->print();
     }
-    return Processes[0];
 }
 
-int main(){
-    Process* Processes;
-    Processes = fileReader();
-    
+int main()
+{
+    fstream fin;
+    fin.open("in1.txt", fstream::in);
+    fin >> NUM_PROCESS;
+    cout << NUM_PROCESS << endl;
+    Process *Processes[NUM_PROCESS];
+
+    fileReader(fin, Processes);
+    Processes[0]->print();
     
     return 0;
     
